f098d9bd8573dbe95d5358d9b49bcef7a201c6d4
r-kubot <r-kubota@i-enter.co.jp>
2023-04-27 11:08:03 +0900

    pod installとgitignoreの作成まで実施

--- taskapp/ViewController.swift
+++ taskapp/ViewController.swift
@@ -7,13 +7,62 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.fillerRowHeight = UITableView.automaticDimension
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+//    データの数（＝セルの数）を返す
+//    tableView(_:numberOfRowsInSection:)は、UITableViewDataSourceプロトコルのメソッド
+    
+    func tableView(_ tableview: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
     }
 
-
+    
+//    各セルの内容を返す
+//    tableView(_:cellForRowAt:)は、UITableViewDataSourceプロトコルのメソッド
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        再利用可能なcellを得る
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        return cell
+    }
+    
+    
+//    各セルを選択した時に実行されるメソッド
+//   tableView(_:didSelectRowAt:)は、 UITableViewDelegateプロトコルのメソッド
+//    セルタップ→タスク入力画面に遷移させる
+    
+    func tableView(_ tableView: UITableView,didSelectRowAt IndexPath: IndexPath) {
+        performSegue(withIdentifier: "cellSegue", sender: nil)
+//        ↑追加。segueのIDを指定して遷移させる
+    }
+    
+    
+//    セルが削除可能なことを伝えるメソッド
+//    tableView(_:editingStyleForRowAt:)　は、UITableViewDelegateプロトコルのメソッド
+    
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+    
+        return .delete
+    }
+    
+    
+//    Deleteボタンが押された時のメソッド
+//    tableView(_:commit:forRowAt:)　は、UITableViewDataSourceプロトコルのメソッド
+//    Deleteが押された際にデータを削除する
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt IndexPath: IndexPath) {
+    }
+   
 }
+
 
